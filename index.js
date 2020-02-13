023889066b420fcd69c6837c872442789ecc3399
jhryniw <jhryniw@gmail.com>
2020-02-12 18:06:55 -0700

    don't load images, use one tab

--- index.js
+++ index.js
@@ -15,27 +15,31 @@ function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-async function download_recipe(browser, recipeId) {
-  const page = await browser.newPage();
+async function download_recipe(page, recipeId) {
   await page.goto(recipeBaseUrl + recipeId.toString(), {waitUntil: 'networkidle2'});
   const html = await page.content();
   fs.writeFile(dirName + '/' + recipeId + '.html', html, () => {
     console.log(recipeId + ' Saved!');
   });
-  return page.close();
 }
 
 (async () => {
 
   const browser = await puppeteer.launch({headless: false});
+  const page = await browser.newPage();
+
+  await page.setRequestInterception(true);
+  page.on('request', request => {
+    if (request.resourceType() === 'image')
+      request.abort();
+    else
+      request.continue();
+  });
 
     try {
       for (let i = startId; i < endId; i++) {
-        await download_recipe(browser, i);
-
+        await download_recipe(page, i);
       }
-
-      await Promise.all(downloads);
     } catch (err) {
       console.log(err);
     } finally {
